bb434a7beee74b413cdc4ce908fa62a2cbcc4c01
eman <emmanuel-adjei@outlook.com>
2021-09-15 16:21:57 +0100

    Student JSON object returned

--- src/main/java/com/example/SpringBootDemo/SpringBootDemoApplication.java
+++ src/main/java/com/example/SpringBootDemo/SpringBootDemoApplication.java
@@ -1,9 +1,16 @@
 package com.example.SpringBootDemo;
 
+import com.example.SpringBootDemo.student.Student;
+import com.example.SpringBootDemo.student.StudentImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.Period;
+import java.util.List;
 
 @SpringBootApplication
 @RestController
@@ -14,8 +21,16 @@ public class SpringBootDemoApplication {
 	}
 
 	@GetMapping
-	public String hello() {
-		return "Hello World";
+	public List<Student> studentList() {
+		return List.of(
+				new StudentImpl(
+						1L,
+						"Michael",
+						"michael.scott@dundermiflin.com",
+						LocalDate.of(1964, Month.FEBRUARY, 20),
+						Period.between(LocalDate.of(1964, Month.FEBRUARY, 20), LocalDate.now()).getYears()
+				)
+		);
 	}
 
 }
